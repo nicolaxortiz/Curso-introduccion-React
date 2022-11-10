9ecb542e6f576f729bdcae4af81a475e2abb09fa
nicolaxortiz <nicolaxortiz@gmail.com>
2022-11-10 18:59:51 -0500

    Vistas de los complete/incomplete mejoradas

--- src/components/BlankFilter.js
+++ src/components/BlankFilter.js
@@ -1,13 +1,25 @@
 import React from "react";
 
-function BlankFilter(){
+function BlankFilter(props){
+
+    let title, msg, img;
+
+    if(props.getFilter == "Completed"){
+        title = "¿Qué paso?";  
+        msg = "Parece que no has completado ninguna tarea aun";  
+        img = "https://i.imgur.com/UjXFNb6.png"    
+    }else{
+        title = "¡Muy bien!";  
+        msg = "Ya has completado todas tus tareas!";  
+        img = "https://i.imgur.com/sqNgsjN.png"
+    }
     return(
         <React.Fragment>
-            <h1 className="emptyTitle">¿Qué paso?</h1>
+            <h1 className="emptyTitle">{title}</h1>
 
-            <img src="https://i.imgur.com/sqNgsjN.png" className="imgEmpty"></img>
+            <img src={img} className="imgEmpty"></img>
 
-            <h2 className="empty-p">Parece que ninguna tarea cumple con el filtro</h2>
+            <h2 className="empty-p">{msg}</h2>
         </React.Fragment>
     )
 }
